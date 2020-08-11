358db003abe68f02fd9d97efc0bf303b97b9c4c7
wskort <w.s.vanderkallen@gmail.com>
2020-08-11 10:12:28 +0200

    Changed passwords to match complexity requirements and added test for insufficient complexity.

--- __tests__/account.js
+++ __tests__/account.js
@@ -5,7 +5,7 @@ const request = supertest(app)
 test('Account: Change Password', async done => {
   const resToken = await request
     .post('/login')
-    .send({ email: 'password@imp.com', password: 'GoGoEstera' })
+    .send({ email: 'password@imp.com', password: 'G0G0!Estera' })
     .set('Accept', 'application/json')
   expect(resToken.status).toBe(200)
 
@@ -16,18 +16,26 @@ test('Account: Change Password', async done => {
     .expect('Content-Type', /json/)
   expect(resAuth.status).toBe(200)
 
-  const res = await request
+  const resWrong = await request
     .post('/user/password')
     .set('Accept', 'application/json')
     .set('Authorization', 'Bearer ' + resAuth.body.token)
     .send({ password: 'EsteraGoGo' })
+    .expect('Content-Type', /json/)
+  expect(resWrong.status).toBe(400)
+
+  const res = await request
+    .post('/user/password')
+    .set('Accept', 'application/json')
+    .set('Authorization', 'Bearer ' + resAuth.body.token)
+    .send({ password: 'EsteraG0G0!' })
     .expect('Content-Type', /json/)
   expect(res.status).toBe(200)
 
   setTimeout(async () => {
     const resTokenB = await request
       .post('/login')
-      .send({ email: 'password@imp.com', password: 'EsteraGoGo' })
+      .send({ email: 'password@imp.com', password: 'EsteraG0G0!' })
       .set('Accept', 'application/json')
     expect(resTokenB.status).toBe(200)
 
@@ -42,7 +50,7 @@ test('Account: Change Password', async done => {
       .post('/user/password')
       .set('Accept', 'application/json')
       .set('Authorization', 'Bearer ' + resAuthB.body.token)
-      .send({ password: 'GoGoEstera' })
+      .send({ password: 'G0G0!Estera' })
       .expect('Content-Type', /json/)
     expect(resB.status).toBe(200)
 
